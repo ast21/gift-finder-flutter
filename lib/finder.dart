61685e659e4afc0db02e541c0f17b2def4226042
Anastas Mironov <ast@l80.ru>
2022-03-05 02:26:42 +0900

    CU-2700x5y add filters in finder page

--- lib/finder.dart
+++ lib/finder.dart
@@ -1,12 +1,89 @@
 import 'package:flutter/material.dart';
 
-class Finder extends StatelessWidget {
+class Finder extends StatefulWidget {
   const Finder({Key? key}) : super(key: key);
 
   @override
+  State<Finder> createState() => _FinderState();
+}
+
+class _FinderState extends State<Finder> {
+  var _gender = 'M';
+  RangeValues _PriceValues = const RangeValues(501, 10000);
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('This Finder')),
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.only(left: 20, right: 20),
+        child: Center(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text('Кому дарим подарок?'),
+                Row(
+                  children: [
+                    Expanded(
+                        child: Row(
+                      children: [
+                        Radio(
+                            groupValue: _gender,
+                            onChanged: (value) => {
+                                  setState(() {
+                                    _gender = value.toString();
+                                  })
+                                },
+                            value: 'M'),
+                        const Text('Мужской'),
+                      ],
+                    )),
+                    Expanded(
+                        child: Row(
+                      children: [
+                        Radio(
+                            groupValue: _gender,
+                            onChanged: (value) => {
+                                  setState(() {
+                                    _gender = value.toString();
+                                  })
+                                },
+                            value: 'F'),
+                        const Text('Женский'),
+                      ],
+                    ))
+                  ],
+                ),
+                const TextField(decoration: InputDecoration(border: OutlineInputBorder(), labelText: 'Возраст')),
+                const TextField(decoration: InputDecoration(border: OutlineInputBorder(), labelText: 'Выберите хобби')),
+                Row(
+                  children: const [
+                    Expanded(
+                        child: TextField(
+                            decoration: InputDecoration(border: OutlineInputBorder(), labelText: 'от 500 ₽'))),
+                    Expanded(
+                        child: TextField(
+                            decoration: InputDecoration(border: OutlineInputBorder(), labelText: 'до 10000 ₽'))),
+                  ],
+                ),
+                RangeSlider(
+                  values: _PriceValues,
+                  max: 10000,
+                  min: 500,
+                  divisions: 19,
+                  labels: RangeLabels(
+                    _PriceValues.start.round().toString(),
+                    _PriceValues.end.round().toString(),
+                  ),
+                  onChanged: (RangeValues values) {
+                    setState(() {
+                      _PriceValues = values;
+                    });
+                  },
+                ),
+              ]),
+        ),
+      ),
     );
   }
 }
